e449f8c21394bf53b595449a97f63b7470119f0b
Adam Parker <mr.parker.a@outlook.com>
2019-02-08 13:36:25 +0000

    Pure Xamarin Forms Tip Calc working

---
+++ Xamarin Forms/XamarinForms/XamarinForms/XamarinForms/ViewModels/TipViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace XamarinForms.ViewModels
+{
+    public class TipViewModel : BaseViewModel
+    {
+        public TipViewModel()
+        {
+            Generosity = 50;
+        }
+
+        private double _amount;
+        private int _generosity;
+        private double _tip;
+
+        public double Amount { get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged();
+                Recalculate();
+            }
+        }
+
+        private void Recalculate()
+        {
+            Tip = Amount * ((double)Generosity / 100);
+        }
+
+        public int Generosity
+        {
+            get => _generosity;
+            set
+            {
+                _generosity = value;
+                OnPropertyChanged();
+                Recalculate();
+            }
+        }
+
+
+        public double Tip
+        {
+            get => _tip;
+            set
+            {
+                _tip = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Xamarin Forms/XamarinForms/XamarinForms/XamarinForms/Views/TipView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinForms.ViewModels;
+
+namespace XamarinForms.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TipView : ContentPage
+	{
+		public TipView ()
+		{
+            BindingContext = new TipViewModel();
+			InitializeComponent ();
+		}
+	}
+}
